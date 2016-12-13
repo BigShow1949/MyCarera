edd46442038721446246ee951df2982dd9401d2b
BigShow1949 <1029883589@qq.com>
2016-12-13 17:34:53 +0800

    Initial Commit

---
+++ MyCarera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCarera
+//
+//  Created by apple on 16/12/13.
+//  Copyright © 2016年 智慧互通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyCarera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyCarera
+//
+//  Created by apple on 16/12/13.
+//  Copyright © 2016年 智慧互通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
