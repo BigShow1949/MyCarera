240371fa12c95e491678406edf41142e0e55e8ca
BigShow1949 <1029883589@qq.com>
2016-12-14 14:30:51 +0800

    我的相机

--- MyCarera/ViewController.h
+++ MyCarera/ViewController.h
@@ -7,9 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ViewController : UIViewController
 
-
+/**
+ *  AVCaptureSession对象来执行输入设备和输出设备之间的数据传递
+ */
+@property (nonatomic, strong) AVCaptureSession* session;
+/**
+ *  输入设备
+ */
+@property (nonatomic, strong) AVCaptureDeviceInput* videoInput;
+/**
+ *  照片输出流
+ */
+@property (nonatomic, strong) AVCaptureStillImageOutput* stillImageOutput;
+/**
+ *  预览图层
+ */
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer* previewLayer;
 @end
 
--- MyCarera/ViewController.m
+++ MyCarera/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
+#define YFScreen [UIScreen mainScreen].bounds.size
+
 @interface ViewController ()
+@property (nonatomic, strong) UIView *backView;
 
 @end
 
@@ -16,14 +19,139 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self UIAbout];
+    [self initAVCaptureSession];
+
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [super viewWillAppear:YES];
+    
+    if (self.session) {
+        [self.session startRunning];
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidDisappear:(BOOL)animated{
+    
+    [super viewDidDisappear:YES];
+    
+    if (self.session) {
+        [self.session stopRunning];
+    }
 }
 
+
+
+- (void)initAVCaptureSession{
+    
+    //捕获设备，通常是前置摄像头，后置摄像头，麦克风（音频输入）
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    //更改这个设置的时候必须先锁定设备，修改完后再解锁，否则崩溃
+    [device lockForConfiguration:nil];
+    [device setFlashMode:AVCaptureFlashModeAuto];//设置闪光灯为自动
+    [device unlockForConfiguration];
+    
+    // 初始化输入设备
+    NSError *error;
+    self.videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:device error:&error];
+    if (error) {
+        NSLog(@"%@",error);
+    }
+    // 图片输出
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    //输出设置。AVVideoCodecJPEG   输出jpeg格式图片
+    NSDictionary * outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG,AVVideoCodecKey, nil];
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    
+    //session：由他把输入输出结合在一起，并开始启动捕获设备（摄像头
+    self.session = [[AVCaptureSession alloc] init];
+    if ([self.session canAddInput:self.videoInput]) {
+        [self.session addInput:self.videoInput];
+    }
+    if ([self.session canAddOutput:self.stillImageOutput]) {
+        [self.session addOutput:self.stillImageOutput];
+    }
+    
+    //初始化预览图层
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    // AVLayerVideoGravityResizeAspect
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    self.previewLayer.frame = CGRectMake(0, 0,YFScreen.width, YFScreen.height - 64);
+    
+    self.backView.layer.masksToBounds = YES;
+    [self.backView.layer addSublayer:self.previewLayer];
+    
+}
+
+- (void)UIAbout {
+
+    UIButton *backBtn = [[UIButton alloc] init];
+    backBtn.frame = CGRectMake(20, YFScreen.height-44, 50, 44);
+    [backBtn setTitle:@"后" forState:UIControlStateNormal];
+    [backBtn addTarget:self action:@selector(backBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    backBtn.backgroundColor = [UIColor redColor];
+    [self.backView addSubview:backBtn];
+    
+    UIButton *frontBtn = [[UIButton alloc] init];
+    frontBtn.frame = CGRectMake(90, YFScreen.height-44, 50, 44);
+    [frontBtn setTitle:@"前" forState:UIControlStateNormal];
+    [frontBtn addTarget:self action:@selector(frontBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    frontBtn.backgroundColor = [UIColor redColor];
+    [self.backView addSubview:frontBtn];
+    
+    UIButton *takePhotoBtn = [[UIButton alloc] init];
+    takePhotoBtn.frame = CGRectMake(160, YFScreen.height-44, 50, 44);
+    [takePhotoBtn setTitle:@"Photo" forState:UIControlStateNormal];
+    [takePhotoBtn addTarget:self action:@selector(takePhotoBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    takePhotoBtn.backgroundColor = [UIColor redColor];
+    [self.backView addSubview:takePhotoBtn];
+}
+
+#pragma mark - even response
+- (void)backBtnClick:(UIButton *)btn {
+    [self changeCameraWithPosition:AVCaptureDevicePositionBack];
+}
+
+- (void)frontBtnClick:(UIButton *)btn {
+    [self changeCameraWithPosition:AVCaptureDevicePositionFront];
+}
+
+- (void)takePhotoBtnClick:(UIButton *)btn {
+
+
+}
+
+#pragma mark - private
+- (void)changeCameraWithPosition:(AVCaptureDevicePosition )desiredPosition {
+    for (AVCaptureDevice *d in [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
+        NSLog(@"d = %@", d);
+        if ([d position] == desiredPosition) {
+            [self.previewLayer.session beginConfiguration];
+            AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:d error:nil];
+            for (AVCaptureInput *oldInput in self.previewLayer.session.inputs) {
+                [[self.previewLayer session] removeInput:oldInput];
+            }
+            [self.previewLayer.session addInput:input];
+            [self.previewLayer.session commitConfiguration];
+            break;
+        }
+    }
+}
+
+#pragma mark - 懒加载
+- (UIView *)backView {
+
+    if (!_backView) {
+        _backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, YFScreen.width, YFScreen.height)];
+        _backView.backgroundColor = [UIColor lightGrayColor];
+        [self.view addSubview:_backView];
+    }
+    return _backView;
+}
 
 @end
